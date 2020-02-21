dc0daab242d8b32f04338ae2a476684ad3ea127f
Igor Thierry <igorthierry15@gmail.com>
2020-02-20 21:50:40 -0300

    ajustes API

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -59,7 +60,7 @@ class UserController {
     const user = await User.findByPk(req.userId);
 
     // se o usuer está trocando de email, verifica se o email digitado ja existe
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
@@ -72,13 +73,23 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
       name,
       email,
-      provider,
+      avatar,
     });
   }
 }
